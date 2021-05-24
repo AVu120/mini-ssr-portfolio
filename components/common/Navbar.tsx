6dd2ab4491eef4aa91f5f5be92519453f7cc3a9a
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-05-25 06:49:10 +1000

    Add routing to navbar.

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,13 +1,23 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
     <nav>
       <div className="logo">
         <h1>Ninja List</h1>
       </div>
-      <a>Home</a>
-      <a>Skills</a>
-      <a>Experience</a>
-      <a>Projects</a>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/skills">
+        <a>Skills</a>
+      </Link>
+      <Link href="/experience">
+        <a>Experience</a>
+      </Link>
+      <Link href="/projects">
+        <a>Projects</a>
+      </Link>
     </nav>
   );
 };
