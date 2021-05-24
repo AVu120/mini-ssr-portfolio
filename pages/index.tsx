d58e80895b52557329e5c1f4372f2c30072797ce
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-05-25 08:54:58 +1000

    Add styles to home page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,25 @@
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, sed
         maiores? Nemo beatae minima impedit, iste quo temporibus velit cumque
         perferendis quisquam alias ipsa assumenda, consequuntur id blanditiis
         aliquam totam!
       </p>
-      <p>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, sed
         maiores? Nemo beatae minima impedit, iste quo temporibus velit cumque
         perferendis quisquam alias ipsa assumenda, consequuntur id blanditiis
         aliquam totam!
       </p>
+      <Link href="/skills">
+        <a className={styles.btn}>Check out my skills!</a>
+      </Link>
     </div>
   );
 }
