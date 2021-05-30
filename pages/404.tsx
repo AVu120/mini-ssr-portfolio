06b7ad6edf17843ade61f01426731f45c98ab6dc
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-05-31 08:19:13 +1000

    Automatically redirect user to home-page after 3 seconds.

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,17 +1,38 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
 import styles from "./404.module.css";
 
 const NotFound = () => {
+  const [count, setCount] = useState(3);
+  const router = useRouter();
+
+  // Automatically redirect user to home-page after 3 seconds.
+  useEffect(() => {
+    const automaticRedirect = setTimeout(() => {
+      router.push("/");
+    }, 3000);
+    const countDown = setInterval(() => {
+      setCount((preVal) => preVal - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(countDown);
+      clearTimeout(automaticRedirect);
+    };
+  }, []);
+
   return (
     <div>
       <div className={styles.not_found}>
         <h1>Ooooops...</h1>
         <h2>That page cannot be found.</h2>
         <p>
-          Go back to the{" "}
+          Redirecting you to the{" "}
           <Link href="/">
             <a className={styles.homepage_link}>Homepage</a>
-          </Link>
+          </Link>{" "}
+          in {count}.
         </p>
       </div>
     </div>
