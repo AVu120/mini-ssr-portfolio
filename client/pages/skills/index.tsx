d187df2dd8bd81fc065049311c436d62676c09ed
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-06-06 08:19:10 +1000

    Render skills page on server with skills data before sending it to client.

--- client/pages/skills/index.tsx
+++ client/pages/skills/index.tsx
@@ -1,7 +1,31 @@
+export const getStaticProps = async () => {
+  try {
+    const response = await fetch("http://localhost:5000/skills");
+    const data = await response.json();
+
+    return {
+      props: {
+        skills: data,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 import Head from "next/head";
+import commonStyles from "../common.module.css";
 import styles from "./Skills.module.css";
 
-const Skills = () => {
+interface ISkill {
+  name: string;
+  url: string;
+}
+interface IProps {
+  skills: [ISkill];
+}
+
+const Skills = ({skills}: IProps) => {
   return (
     <>
       <Head>
@@ -9,18 +33,15 @@ const Skills = () => {
       </Head>
       <div>
         <h1 className={styles.title}>Skills</h1>
-        <p className={styles.text}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, rerum,
-          autem excepturi eius illum provident, cum labore quisquam tempora
-          aliquam iure magni distinctio error veritatis voluptas? Eaque possimus
-          illo quibusdam.
-        </p>
-        <p className={styles.text}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, rerum,
-          autem excepturi eius illum provident, cum labore quisquam tempora
-          aliquam iure magni distinctio error veritatis voluptas? Eaque possimus
-          illo quibusdam.
-        </p>
+        <ul>
+          {skills.map((skill) => (
+            <li key={`skill-${skill.name}-${skill.url}`}>
+              <a href={skill.url}>
+                <h3 className={commonStyles.list_item}>{skill.name}</h3>
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
+import skills from "../db/skills.json";
 
 const app = express();
 app.disable("x-powered-by");
 const port = 5000;
 
+app.get("/", (req, res) => res.status(200).send("OK"));
 app.get("/skills", (req, res) => {
-  const skills = [];
+  res.send(skills);
 });
 app.get("/projects", (req, res) => res.status(200).send("projects"));
 
